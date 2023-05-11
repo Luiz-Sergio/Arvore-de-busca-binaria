570d53e2e4bdf0ab70cc98600d34d1a1a4fd8627
Luiz-Sergio <luiz.sergio.dev@gmail.com>
2023-05-11 14:23:06 -0300

    Criando template para ler arquivo input

---
+++ readerTemplate.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+
+    while (cin >> s)
+    {
+        if (s == "CHEIA")
+        {
+            // call function isCheia
+        }
+        else if (s == "INSERIR")
+        {
+            // call function inserir
+        }
+    }
+    cout << endl;
+    return 0;
+}
