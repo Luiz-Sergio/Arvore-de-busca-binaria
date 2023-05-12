b6f9e1e62cd10179947e586193ad3c2cc105b4a2
Luiz-Sergio <luiz.sergio.dev@gmail.com>
2023-05-12 18:02:56 -0300

    Initial tree setup

--- readerTemplate.cpp
+++ readerTemplate.cpp
@@ -1,39 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Node
+{
+    int key;
+    struct node *left;
+    struct node *right;
+    int nivel;
+
+    Node(int k)
+    {
+        key = k;
+    }
+
+    void insert()
+    {
+    }
+
+    void remove()
+    {
+    }
+
+    void print()
+    {
+    }
+};
+
+bool is_number(const std::string &s);
+
 int main()
 {
     string s;
+    int numbers;
 
     while (cin >> s)
     {
-        if (s == "CHEIA")
+        // fill the three here
+        if (is_number(s))
         {
-            // call function isCheia
+            numbers = stoi(s);
+            cout << numbers;
         }
-        else if (s == "MEDIANA")
-        {
-            // call function mediana
-        }else if (s == "COMPLETA")
-        {
-
-        }else if (s == "ENESIMO")
-        {
-            
-        }else if (s == "POSICAO")
-        {
-            
-        }else if (s == "MEDIANA")
-        {
-            
-        }else if (s == "IMPRIMA")
-        {
-            
-        }else if (s == "REMOVA")
-        {
-            
+        else
+        { // read commands here
+            if (s == "CHEIA")
+            {
+                // call function isCheia
+                cout << "Cheia" << endl;
+            }
+            else if (s == "MEDIANA")
+            {
+                // call function mediana
+            }
+            else if (s == "COMPLETA")
+            {
+                cout << "completa" << endl;
+            }
+            else if (s == "ENESIMO")
+            {
+            }
+            else if (s == "POSICAO")
+            {
+            }
+            else if (s == "MEDIANA")
+            {
+            }
+            else if (s == "IMPRIMA")
+            {
+            }
+            else if (s == "REMOVA")
+            {
+            }
         }
     }
-    cout << endl;
+
     return 0;
+}
+
+bool is_number(const std::string &s)
+{
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && std::isdigit(*it))
+        ++it;
+    return !s.empty() && it == s.end();
 }
